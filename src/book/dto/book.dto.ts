a37e6cc6e6ea4db01ad9e9d7d40d6d5288bf71ee
chiefnaheem <naheemademola21@gmail.com>
2023-06-14 09:25:12 -0700

    added swagger doc

--- src/book/dto/book.dto.ts
+++ src/book/dto/book.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+
 export class BookDto {
+    @ApiProperty({ example: 'Mars', description: 'The title of the book' })
   title: string;
+  @ApiPropertyOptional({ example: 'available', description: 'The status of the book' })
   bookStatus?: string;
+    @ApiPropertyOptional({ example: 'The book is about Mars', description: 'The description of the book' })
   description?: string;
+    @ApiProperty({ example: 'John Doe', description: 'The author of the book' })
   author: string;
+    @ApiPropertyOptional({ example: 'Mars Publishing', description: 'The publisher of the book' })
   publisher?: string;
+    @ApiProperty({ example: 2021, description: 'The year of the book' })
   year: number;
+    @ApiProperty({ example: 100, description: 'The pages of the book' })
   pages: number;
 }
 
