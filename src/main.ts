73aa9644ed637d5a87fc1307536082fa7be145e3
chiefnaheem <naheemademola21@gmail.com>
2023-06-14 06:38:35 -0700

    feat: set up documentation and the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import rateLimit from 'express-rate-limit';
+import { setupSwagger } from './api-docs.swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+   // protect app from brute-force attacks
+   app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  setupSwagger(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
