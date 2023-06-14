3731195d060368d786185776c876a7b7ae674857
chiefnaheem <naheemademola21@gmail.com>
2023-06-14 09:09:23 -0700

    update book controller

--- src/book/controller/book.controller.ts
+++ src/book/controller/book.controller.ts
@@ -3,7 +3,7 @@ import { IResponse } from "../book.interface";
 import { BookDto, PaginationDto } from "../dto/book.dto";
 import { ObjectValidationPipe } from "../pipe/validation.pipe";
 import { BookService } from "../service/book.service";
-import { createBookValidator, paginationValidator } from "../validator/book.validator";
+import { createBookValidator, paginationValidator, updateBookValidator } from "../validator/book.validator";
 
 @Controller('book')
 export class BookController {
@@ -32,6 +32,15 @@ export class BookController {
         return {
             status: HttpStatus.OK,
             message: 'Book retrieved successfully',
+            data: book,
+        };
+    }
+
+    async updateBook(@Param('id', ParseUUIDPipe) id: string, @Body(new ObjectValidationPipe(updateBookValidator)) data: BookDto): Promise<IResponse> {
+        const book = await this.bookService.updateBook(id, data);
+        return {
+            status: HttpStatus.OK,
+            message: 'Book updated successfully',
             data: book,
         };
     }
