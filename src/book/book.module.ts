cf9d35ab4a862527bfa2fa3e3d97182e9a875d06
chiefnaheem <naheemademola21@gmail.com>
2023-06-14 08:26:26 -0700

    feat: book controller

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Book } from "src/entities/book.entities";
+import { BookController } from "./controller/book.controller";
 import { BookService } from "./service/book.service";
 
 @Module({
